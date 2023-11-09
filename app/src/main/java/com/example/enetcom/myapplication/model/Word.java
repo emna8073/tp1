259b05f69f57a082a1f9934508c46a4006ce8793
POST6 <post6.prog2@enetcom.usf.tn>
2023-11-09 16:46:10 +0100

    firstcommit

---
+++ app/src/main/java/com/example/enetcom/myapplication/model/Word.java
@@ -0,0 +1,17 @@
+package com.example.enetcom.myapplication.model;
+
+public class Word {
+    private String mWord;
+
+    public Word(String word){
+        this.mWord = word;
+    }
+
+    public void setWord(String word) {
+        this.mWord = word;
+    }
+
+    public String getWord() {
+        return mWord;
+    }
+}
